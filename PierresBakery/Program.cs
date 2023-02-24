84e18ec32b4ef561e89228dbea83edd3d3dc1b64
Vera Weikel <vera.weikel@gmail.com>
2023-02-24 14:31:01 -0800

    Update bread info on Program.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -9,7 +9,14 @@ namespace PierresBakery
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Pierre's Bakery app!");
-      // Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+      Console.WriteLine("Pierre offers the following deal for Bread: Buy 2, get 1 free. A single loaf costs $5, then:.");
+      Console.WriteLine("two loaves costs $10");
+      Console.WriteLine("three loaves cost $10");
+      Console.WriteLine("four loaves costs $15");
+      Console.WriteLine("five loaves cost $20");
+      Console.WriteLine("six loaves costs $20");
+      Console.WriteLine("seven loaves cost $25");
+      Console.WriteLine("Can you see a pattern? Every 3rd loaf of bread is free.");
       // Console.WriteLine("Please enter a number:");
       // string stringNumber1 = Console.ReadLine();  
       // Console.WriteLine("Enter another number:");
