63b4e6ce1ec44c4bdec258147973876a27e890cc
Vera Weikel <vera.weikel@gmail.com>
2023-02-24 14:02:58 -0800

    Update welcome Program.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,8 +7,8 @@ namespace PierresBakery
   {
     static void Main()
     {
-      // Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      // Console.WriteLine("Welcome to the Shape Tracker app!");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Pierre's Bakery app!");
       // Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
       // Console.WriteLine("Please enter a number:");
       // string stringNumber1 = Console.ReadLine();  
