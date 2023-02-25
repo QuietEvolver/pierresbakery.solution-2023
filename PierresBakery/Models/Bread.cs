21a30d3c18c643b3f6c254002bcb556ca6046323
Vera Weikel <vera.weikel@gmail.com>
2023-02-24 16:51:14 -0800

    WIP: bread model.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -3,10 +3,13 @@ namespace PierresBakery.Models
   public class Bread
   {
     Dictionary<string, string> breads = new Dictionary<string, string>()
-{
-    {"1", "one"},
-    {"2", "two"},
-    {"3", "three"}
-};
+    {
+        {"1", "one"},
+        {"2", "two"},
+        {"3", "three"}
+    };
+
+    // buy 2 get 1 free == %3 
+    //  count by  2; .next()?
   }  
 }
