1807f363dc867b0cd9efe90d228a80a996ffe1be
Vera Weikel <vera.weikel@gmail.com>
2023-02-24 16:20:19 -0800

    Add bread dictionary.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,6 +2,11 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    // Bread code
+    Dictionary<string, string> breads = new Dictionary<string, string>()
+{
+    {"1", "one"},
+    {"2", "two"},
+    {"3", "three"}
+};
   }  
 }
