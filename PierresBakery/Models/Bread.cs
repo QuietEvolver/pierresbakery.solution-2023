4ca89261d9c0f3ef62cc4a68254234067270345a
Vera Weikel <vera.weikel@gmail.com>
2023-02-25 20:13:28 -0800

    Comment out Dictionary.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,12 +2,12 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    Dictionary<string, string> breads = new Dictionary<string, string>()
-    {
-        {"1", "one"},
-        {"2", "two"},
-        {"3", "three"}
-    };
+    // Dictionary<string, string> breads = new Dictionary<string, string>()
+    // {
+    //     {"1", "one"},
+    //     {"2", "two"},
+    //     {"3", "three"}
+    // };
 
     // buy 2 get 1 free == %3 
     //  count by  2; .next()?
