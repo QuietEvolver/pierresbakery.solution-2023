5200da625ace85241879d1b8bd5758b76aa25e0a
Vera Weikel <vera.weikel@gmail.com>
2023-02-26 21:15:04 -0800

    Add fmla.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -8,5 +8,34 @@ namespace PierresBakery.Models
     {
       BreadNumber = 0;
     }
+
+    public Bread( int userBreadInput)
+    {
+      BreadNumber = userBreadInput;
+    }
+
+    public int TotalBreadPrice3for2() 
+    {
+      return (BreadNumber * 5) - (5 * 2); // (BreadNumber * price) - (price * 2)
+    }
+    // public string CheckType()
+    // {
+    //   if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2))) 
+    //   {
+    //     return "not a triangle";
+    //   } 
+    //   else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3))) 
+    //   {
+    //     return "scalene triangle";
+    //   }
+    //   else if ((Side1 == Side2) && (Side1 == _side3)) 
+    //   {
+    //     return "equilateral triangle";
+    //   } 
+    //   else
+    //   {
+    //     return "isosceles triangle";
+    //   }
+    // }
   }  
 }
