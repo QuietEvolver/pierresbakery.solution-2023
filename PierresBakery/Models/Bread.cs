63da8d6002e171fddd67473ffaba9af514f58c14
Vera Weikel <vera.weikel@gmail.com>
2023-02-26 20:12:55 -0800

    add an auto-implemented property to bread.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,14 +2,6 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    // Dictionary<string, string> breads = new Dictionary<string, string>()
-    // {
-    //     {"1", "one"},
-    //     {"2", "two"},
-    //     {"3", "three"}
-    // };
-
-    // buy 2 get 1 free == %3 
-    //  count by  2; .next()?
+    public int breadNumber { get; set; }
   }  
 }
