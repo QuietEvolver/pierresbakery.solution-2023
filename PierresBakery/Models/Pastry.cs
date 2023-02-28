6bb4a6f1c10ce650a87213dcfa481c938ec2b376
Vera Weikel <vera.weikel@gmail.com>
2023-02-28 07:07:49 -0800

    Pass: calculate Total.

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -19,13 +19,14 @@ namespace PierresBakery.Models
 
     public int TotalPastryPrice() 
     {
-     return (PastryNumber * 2); // - Bogo4for3(); // (PastryNumber * price) - (price * (n/#disc))
+     return (PastryNumber * 2); 
     }
 
     public int Bogo4for3()
     {
      return (2 * PastryNumber/4); 
     }
+
     public int TotalPastryPrice4for3() 
     {
      return (PastryNumber * 2); // - Bogo4for3(); // (PastryNumber * price) - (price * (n/#disc))
